3833922fb212432a48459b5a65cdcd7e3242a387
lvillasica <lvillasica@lvillasica.(none)>
2011-08-03 13:49:29 +0800

    removed swfobject.js

--- app/models/burndown_chart.rb
+++ app/models/burndown_chart.rb
@@ -51,8 +51,8 @@ class BurndownChart
     @data1_and_dates = []
     @data2_and_dates = []
     dates.each_with_index do |d, i|
-      @data1_and_dates << ["#{d} 00:00", sprint_data[i]]
-      @data2_and_dates << ["#{d} 00:00", ideal_data[i]]
+      @data1_and_dates << ["#{d} 8:00AM", sprint_data[i]]
+      @data2_and_dates << ["#{d} 8:00AM", ideal_data[i]]
     end
     [@data1_and_dates, @data2_and_dates]
   end
